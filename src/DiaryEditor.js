b39473058cdd4e415d90d40d686e3849d817f2e0
rwony <88661435+rwony@users.noreply.github.com>
2022-05-06 23:14:46 +0900

    Add files via upload

---
+++ src/DiaryEditor.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const DiaryEditor = () => {
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+  });
+
+  const handleChangeState = (e) => {
+    console.log(e.target.name);
+    console.log(e.target.value);
+  };
+
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <input name="author" value={state.author} onChange={handleChangeState} />
+      <div>
+        <textarea
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default DiaryEditor;
