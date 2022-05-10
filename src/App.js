7287ca4a9cebf9ff04f865563dfbe4b4ea04f4da
rwony <88661435+rwony@users.noreply.github.com>
2022-05-10 13:04:06 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+import "./App.css";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "려원",
+    content: "하이~ 1",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "료",
+    content: "하이~ 2",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "서함부인",
+    content: "하이~ 3",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const DiaryEditor = () => {
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   const [state, setState] = useState({
     author: "",
     content: "",
@@ -15,16 +18,31 @@ const DiaryEditor = () => {
   };
 
   const handleSubmit = () => {
-    console.log(state);
+    if (state.author.length < 1) {
+      authorInput.current.focus(); //focus
+      return;
+    }
+
+    if (state.content.length < 5) {
+      contentInput.current.focus(); //focus
+      return;
+    }
+
     alert("저장 성공!");
   };
 
   return (
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
-      <input name="author" value={state.author} onChange={handleChangeState} />
+      <input
+        ref={authorInput}
+        name="author"
+        value={state.author}
+        onChange={handleChangeState}
+      />
       <div>
         <textarea
+          ref={contentInput}
           name="content"
           value={state.content}
           onChange={handleChangeState}
