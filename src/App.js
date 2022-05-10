a010d8ea2ace18cb81843c841cb32f66ce0734c6
rwony <88661435+rwony@users.noreply.github.com>
2022-05-10 22:40:24 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 import "./App.css";
 
-const dummyList = [
-  {
-    id: 1,
-    author: "려원",
-    content: "하이~ 1",
-    emotion: 1,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "료",
-    content: "하이~ 2",
-    emotion: 2,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "서함부인",
-    content: "하이~ 3",
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-];
-
 function App() {
+  const [data, setData] = useState([]);
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    // 일기 데이터를 추가할 수 있는 함수
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
+  const onDelete = (targetId) => {
+    console.log(`${targetId}가 삭제 되었습니다.`);
+
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    console.log(newDiaryList);
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList onDelete={onDelete} diaryList={data} />
     </div>
   );
 }
