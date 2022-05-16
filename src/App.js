9c8db55d06a461336164afe17cbbffd14fd5f4a5
rwony <88661435+rwony@users.noreply.github.com>
2022-05-16 16:50:49 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -25,14 +25,21 @@ function App() {
     console.log(`${targetId}가 삭제 되었습니다.`);
 
     const newDiaryList = data.filter((it) => it.id !== targetId);
-    console.log(newDiaryList);
     setData(newDiaryList);
+  };
+
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
   };
 
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList onRemove={onRemove} diaryList={data} />
+      <DiaryList onRemove={onRemove} onEdit={onEdit} diaryList={data} />
     </div>
   );
 }
